410824cb4daabe11b7bc1037aef339a6c999ed73
ZgsNat <nguyenthaithinh9876@gmail.com>
2025-02-10 14:02:44 +0700

    Update

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "YOUR_DEFAULT_TOKEN")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "@YourBotName")
 DB_PATH = os.getenv("DB_PATH", "database/expenses.db")
 
-# Print the loaded values for debugging
-print(f"TOKEN: {TOKEN}")
-print(f"BOT_USERNAME: {BOT_USERNAME}")
-print(f"DB_PATH: {DB_PATH}")
+# # Print the loaded values for debugging
+# print(f"TOKEN: {TOKEN}")
+# print(f"BOT_USERNAME: {BOT_USERNAME}")
+# print(f"DB_PATH: {DB_PATH}")
